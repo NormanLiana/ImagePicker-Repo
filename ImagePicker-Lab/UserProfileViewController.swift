39de627664f1219c963e9de3b5fb42dcd1860205
NormanLiana <liananorman@pursuit.org>
2019-10-02 17:49:35 -0400

    implements image picker

--- ImagePicker-Lab/UserProfileViewController.swift
+++ ImagePicker-Lab/UserProfileViewController.swift
@@ -7,20 +7,49 @@
 //
 
 import UIKit
+import Photos
 
 class UserProfileViewController: UIViewController {
 
     // MARK: Outlets
+    @IBOutlet weak var imgOutlet: UIImageView!
+    @IBOutlet weak var nameLabelOutlet: UILabel!
     
     // MARK: Properties
+    var userImg = UIImage() {
+        didSet {
+            imgOutlet.image = userImg
+        }
+    }
+    
     
     // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // MARK: Actions
+    
+    @IBAction func addPhotoButtonPressed(_ sender: UIBarButtonItem) {
+        let imgPickVC = UIImagePickerController()
+        imgPickVC.delegate = self
+        imgPickVC.sourceType = .photoLibrary
+        
+        present(imgPickVC, animated: true, completion: nil)
+    }
+    
     // MARK: Private Methods
     
+}
 
-
+// MARK: Extensions
+extension UserProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        self.userImg = image
+        nameLabelOutlet.text = "Flower-Power"
+        dismiss(animated: true, completion: nil)
+    }
 }
