1820c56e6e08417578da2cdfb10ee822ef1181c9
NormanLiana <liananorman@pursuit.org>
2019-10-02 16:14:29 -0400

    adds cocoa file for UserProfileVC

---
+++ ImagePicker-Lab/UserProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserProfileViewController.swift
+//  ImagePicker-Lab
+//
+//  Created by Liana Norman on 10/2/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
